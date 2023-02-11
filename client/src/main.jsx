8e2cd1fa27fd2f06c6d37a3038fd20e97780d9a4
matthew-cacchione <matthew.cacchione@gmail.com>
2023-02-11 11:16:05 -0500

    format main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,9 @@ import ReactDOM from "react-dom/client";
 // Import components.
 import App from "./components/App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
