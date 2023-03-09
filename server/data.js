60a5adebd5f053e932a80266b9e1e8339eec9679
matthew-cacchione <matthew.cacchione@gmail.com>
2023-03-09 17:51:46 -0500

    create data

---
+++ server/data.js
@@ -0,0 +1,7 @@
+const people = [
+  { id: 1, name: "george", language: "javascript" },
+  { id: 2, name: "elaine", language: "rust" },
+  { id: 3, name: "jerry", language: "typescript" },
+];
+
+module.exports = { people };
