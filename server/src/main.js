cb73c33ee4f83f517b5b3d3433b1fe1f13602979
matthew-cacchione <matthew.cacchione@gmail.com>
2023-03-28 09:38:34 -0400

    update log to info

--- server/src/main.js
+++ server/src/main.js
@@ -17,4 +17,4 @@ app.get("*", (request, response) => {
     .json({ status: 404, message: "No endpoint found." });
 });
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.info(`Listening on port ${PORT}`));
