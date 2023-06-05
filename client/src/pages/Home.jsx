fa71e15767ec2326953b62069749082a205fae34
matthew-cacchione <matthew.cacchione@gmail.com>
2023-06-05 19:50:29 -0400

    create form

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,29 +3,58 @@ import styled from "styled-components";
 import { useState } from "react";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (event) => {
+    const key = event.target.id;
+    const value = event.target.value;
+
+    setFormData({ ...formData, [key]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch("/api/people", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+  };
 
   return (
-    <>
-      <Title>Home</Title>
-      <Button onClick={() => setCount(count + 1)}>count is {count}</Button>
-    </>
+    <Wrapper>
+      <Title>Please enter your credentials</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input id="username" onChange={handleChange} placeholder="Username" />
+        <Input
+          id="language"
+          onChange={handleChange}
+          placeholder="Programming language"
+        />
+        <Button>Submit</Button>
+      </Form>
+    </Wrapper>
   );
 };
 
 const Button = styled.button`
-  background-color: #1a1a1a;
-  border: 1px solid transparent;
+  background: #ffffff;
+  border: 3px solid transparent;
   border-radius: 8px;
+  color: #000000;
   cursor: pointer;
   font-family: inherit;
-  font-size: 1em;
-  font-weight: 500;
+  font-size: 1.4rem;
+  font-weight: bold;
   padding: 0.6em 1.2em;
   transition: border-color 0.25s;
+  width: 100%;
 
   &:hover {
-    border-color: #646cff;
+    border-color: #007066;
   }
 
   &:focus,
@@ -34,10 +63,40 @@ const Button = styled.button`
   }
 `;
 
+const Form = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.8rem;
+  width: 60%;
+`;
+
+const Input = styled.input`
+  background: #ffffff;
+  border: 3px solid #007066;
+  border-radius: 8px;
+  box-sizing: border-box;
+  color: #000000;
+  font-size: 1.4rem;
+  height: 3em;
+  padding: 0 0.5em;
+  width: 100%;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
 const Title = styled.h1`
   font-size: 3.2rem;
   line-height: 1.1;
   margin: 0.5em 0;
+`;
+
+const Wrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 `;
 
 export default Home;
