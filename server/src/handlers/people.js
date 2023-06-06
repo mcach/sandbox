5ae96f659ac1820d682df8a6ab3c794c081e3f6f
matthew-cacchione <matthew.cacchione@gmail.com>
2023-06-06 08:11:28 -0400

    create languages data

--- server/src/handlers/people.js
+++ server/src/handlers/people.js
@@ -1,3 +1,6 @@
+// Require data.
+const languages = require("../data/languages.json");
+
 // TODO: endpoint handler for creating a new person.
 const createPerson = (request, response) => {
   console.log(request.body);
