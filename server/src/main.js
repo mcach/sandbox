0155fc44b9a31bd382fcc3265e954e00c4fccc85
matthew-cacchione <matthew.cacchione@gmail.com>
2023-10-13 10:10:48 -0400

    setup demo

--- server/src/main.js
+++ server/src/main.js
@@ -1,20 +1,5 @@
-const express = require("express");
+require("dotenv").config();
 
-const PORT = 8000;
+const { SECRET } = process.env;
 
-const app = express();
-
-// Middleware to parse JSON-formatted data
-app.use(express.json());
-
-// Serve static files from the "public" directory
-app.use(express.static("public"));
-
-// Define a catch-all route to handle 404 errors
-app.all("*", (request, response) => {
-  response.status(404).json({ status: 404, message: "No endpoint found." });
-});
-
-app.listen(PORT, () => {
-  console.info(`Server is listening on port ${PORT}`);
-});
+console.log(SECRET);
